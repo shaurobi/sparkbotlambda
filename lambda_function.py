a55e21b902e3a1dfc32b64e83e62396aeca2f967
shaurobi <shaurobi@cisco.com>
2019-02-13 20:35:29 +1100

    Changed code to support WebexTeamsAPI, also tweaked format slightly, removed documentation

--- lambda_function.py
+++ lambda_function.py
@@ -1,19 +1,8 @@
-"""A sample AWS Lambda function that can be used with a Cisco Spark Bot
-for fetching information triggered by Spark messages.
-
-A Spark Webhook can be created to trigger an HTTP POST to a targetUrl
-such as an AWS API Gateway endpoint.
-
-In this example, the Spark message id is extracted from the Webhook triggered POST to AWS (lambda_handler)
- """
-
-from __future__ import print_function
-
 import json
 import os
-from ciscosparkapi import CiscoSparkAPI
+from webexteamssdk import WebexTeamsAPI
 
-api = CiscoSparkAPI(access_token=os.environ['SPARK_ACCESS_TOKEN'])
+api = WebexTeamsAPI()
 
 # the lambda handler function
 def lambda_handler(event, context):
@@ -24,13 +13,17 @@ def lambda_handler(event, context):
     # print the event details received from the Spark Webhook for logging
     print("Received event: " + json.dumps(event, indent=2))
     #check if self generated this event
-    if event['actorId'] == api.people.me().id:
+    print(event['body'].__class__)
+    print(event['body'])
+    message = json.loads(event['body'])
+    me = api.people.me()
+    if message['actorId'] == me.id:
     	print("Ignoring event due to actorId = me")
     	return 0
     # assign the Spark message id to a variable
-    msg_id = event['data']['id']
+    msg_id = message['data']['id']
     # assign the Spark roomId to a variable (for the Bot to respond into the right room dynamically)
-    room_id = event['data']['roomId']
+    room_id = message['data']['roomId']
     
     t = api.messages.get(msg_id)
     if t.text:
@@ -38,5 +31,4 @@ def lambda_handler(event, context):
         api.messages.create(roomId=room_id, text=response)
     else:
         print("Error: No Text Received in msg")
-
     return "Finished"
