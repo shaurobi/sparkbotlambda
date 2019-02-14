40f18c930fe84f56c0e2ea2c0653463673136785
shaurobi <shaurobi@cisco.com>
2019-02-14 15:28:34 +1100

    Tweaked formatting to seperate Camera get as a separate function. Removed references to Spark

--- lambda_function.py
+++ lambda_function.py
@@ -3,19 +3,20 @@ import os
 from webexteamssdk import WebexTeamsAPI
 import requests
 
-
-
 api = WebexTeamsAPI()
 
 # the lambda handler function
+def getCamera():
+    url = "http://api.meraki.com/api/v0/networks/L_602356450160820134/cameras/Q2HV-B63K-AAHS/videoLink"
+    headers = {
+        'x-cisco-meraki-api-key': os.environ['MERAKI_DASHBOARD_KEY'],
+        'cache-control': "no-cache"
+        }
+    result = requests.request("GET", url, headers=headers)
+    response = result.json()
+    return response['url']
+    
 def lambda_handler(event, context):
-    """Demonstrates a simple HTTP endpoint using API Gateway. You have full
-    access to the request and response payload, including headers and
-    status code.
-    """
-    # print the event details received from the Spark Webhook for logging
-    print("Received event: " + json.dumps(event, indent=2))
-    #check if self generated this event
     print(event['body'].__class__)
     print(event['body'])
     message = json.loads(event['body'])
@@ -23,25 +24,19 @@ def lambda_handler(event, context):
     if message['actorId'] == me.id:
         print("Ignoring event due to actorId = me")
         return 0
-    # assign the Spark message id to a variable
+    # assign the WebexTeams message id to a variable
     msg_id = message['data']['id']
-    # assign the Spark roomId to a variable (for the Bot to respond into the right room dynamically)
+    # assign the WebexTeams roomId to a variable (for the Bot to respond into the right room dynamically)
     room_id = message['data']['roomId']
     
     t = api.messages.get(msg_id)
-    t = t.lower()
-    if t.text:
-        if 'camera' in t:
-            url = "http://api.meraki.com/api/v0/networks/L_602356450160820134/cameras/Q2HV-B63K-AAHS/videoLink"
-            headers = {
-                'x-cisco-meraki-api-key': os.environ['MERAKI_DASHBOARD_KEY'],
-                'cache-control': "no-cache"
-                }
-            response = requests.request("GET", url, headers=headers)
-        print(response.text)	
-        else:
+    print(t.text)
+    if 'camera' in t.text:
+        print("Camera message received!")
+        api.messages.create(roomId=room_id, text ="Let me find that camera stream")
+        url = getCamera()
+        api.messages.create(roomId=room_id, text=url)
+    else:
         response = 'NO COMMAND MATCH FOUND: ' + t.text
         api.messages.create(roomId=room_id, text=response)
-    else:
-        print("Error: No Text Received in msg")
     return "Finished"
